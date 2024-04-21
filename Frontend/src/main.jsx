482819f2a5e6f37814a98675717de44957c05617
39matt <matija.todosijevic.i4@gmail.com>
2024-04-21 05:46:05 +0200

    PopQuiz

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import InitialQuiz from './components/InitialQuiz/InitialQuiz.jsx'
+import PopQuiz from './components/PopQuiz/PopQuiz.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
-  { path: '/initial-quiz', element: <InitialQuiz />}
+  { path: '/initial-quiz', element: <InitialQuiz />},
+  { path: '/pop-quiz', element: <PopQuiz />}
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
